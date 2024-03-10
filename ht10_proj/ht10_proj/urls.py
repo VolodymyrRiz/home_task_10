75e419a19685b0e27b8cd953d1ebb421e8300c4e
VolodymyrRiz <142658224+VolodymyrRiz@users.noreply.github.com>
2024-03-10 23:49:05 +0200

    ht10-3

--- ht10_proj/ht10_proj/urls.py
+++ ht10_proj/ht10_proj/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls'))
-    path('users/', include('users.urls'))
+    #path('users/', include('users.urls'))
 ]
 
