abdc1d5b2391106f0bccaf749be9dfb1d68bb63e
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-11 22:50:40 +0200

    ht10-11

--- ht10_proj/ht10_proj/urls.py
+++ ht10_proj/ht10_proj/urls.py
@@ -5,9 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    path("", include('quotes.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('quotes.urls'))
+    path("", include('quotes.urls')),    
     #path('users/', include('users.urls'))
 ]
 
--- ht10_proj/quotes/views.py
+++ ht10_proj/quotes/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, 'quotes.index.html', context={})
+    return render(request, 'quotes/index.html', context={})
