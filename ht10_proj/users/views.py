5cf4eae01a23d84c5f8828c211f32779bd9facea
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-23 00:10:01 +0200

    ht10-36

--- ht10_proj/users/views.py
+++ ht10_proj/users/views.py
@@ -1,16 +1,10 @@
 from django.views import View
 from .forms import RegisterForm, LoginForm
-from django.shortcuts import render, redirect
-
-
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+
 # from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.shortcuts import render, redirect
 
 
 class RegisterView(View):
@@ -36,11 +30,12 @@ class RegisterView(View):
         return render(request, self.template_name, context={"form": form})
     
     def loginuser(request):
-        if request.user.is_authenticated:
-            return redirect(to='quotes:index')
+        if request.user.is_authenticated:            
+            return redirect(to='quotes:main')
 
         if request.method == 'POST':
             user = authenticate(username=request.POST['username'], password=request.POST['password'])
+           
             if user is None:
                 messages.error(request, 'Username or password didn\'t match')
                 return redirect(to='users:login')
