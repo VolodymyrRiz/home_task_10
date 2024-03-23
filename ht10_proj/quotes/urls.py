a0575edcfe503b869995420de66fb94c8a1877e0
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-23 22:53:58 +0200

    ht10-38

--- ht10_proj/quotes/urls.py
+++ ht10_proj/quotes/urls.py
@@ -1,13 +1,3 @@
-
-# from . import views
-# from django.urls import path
-
-# app_name = 'quotes'
-# urlpatterns = [
-#     path('', views.main, name='root')
-# ]
-
-# quotes/urls.py
 from django.urls import path
 from . import views
 
--- ht10_proj/users/urls.py
+++ ht10_proj/users/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# app_name = "users"
-
-# urlpatterns = [
-#     path("signup/", views.RegisterView.as_view(), name="signup"),
-#     path('login/', views.LoginView.as_view(), name='login'),
-# ]
-# users/urls.py
 from django.urls import path
 from . import views
 
