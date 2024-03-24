c6ff1204be3701d5384a18a3344d60528cf14247
VolodymyrRiz <v.v.rizun@gmail.com>
2024-03-24 15:22:22 +0200

    ht10-40

--- ht10_proj/users/urls.py
+++ ht10_proj/users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
 from . import views
+from .forms import LoginForm
 
 app_name = "users"
-
 urlpatterns = [
-    path("signup/", views.RegisterView.as_view(), name="signup"),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('', views.LoginView.as_view(), name='index'), # Додайте цей рядок
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="users/login.html", form_class=LoginForm,
+                                     redirect_authenticated_user=True), name="login"),
+    path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
 ]
--- ht10_proj/users/views.py
+++ ht10_proj/users/views.py
@@ -1,10 +1,8 @@
 from django.views import View
-from .forms import RegisterForm, LoginForm
-from django.contrib import messages
+from .forms import RegisterForm
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
-from django.urls import reverse_lazy
+
+from django.contrib import messages
 
 
 class RegisterView(View):
@@ -13,7 +11,7 @@ class RegisterView(View):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('quotes:main')  # Надіслати користувача на головну сторінку, якщо він вже увійшов
+            return redirect(to="/")
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
@@ -25,52 +23,89 @@ class RegisterView(View):
             form.save()
             username = form.cleaned_data["username"]
             messages.success(request, f"Hello {username}, Your account created successfully")
-            
-            return redirect('quotes:main')  # Надіслати користувача на головну сторінку після успішної реєстрації
+            return redirect(to="users:login")
         return render(request, self.template_name, context={"form": form})
 
-    def register(request):
-        if request.method == 'POST':
-            form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to the login page after successful registration
-        else:
-            form = RegisterForm()
-        return render(request, 'users/signup.html', {'form': form})
+# from django.views import View
+# from .forms import RegisterForm, LoginForm
+# from django.contrib import messages
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.forms import AuthenticationForm
+# from django.urls import reverse_lazy
+# from django.contrib.auth.decorators import login_required
 
-class LoginView(View):
-    
-    def index(request):
-        return render(request, 'index.html')
-    
-    def get(self, request):
-        if request.user.is_authenticated:            
-            return redirect('quotes:main')
-        return render(request, 'users/login.html', context={"form": LoginForm()})
 
-    def post(self, request):
-        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+# class RegisterView(View):
+#     template_name = "users/signup.html"
+#     form_class = RegisterForm
+
+#     def dispatch(self, request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return redirect('quotes:main')  # Надіслати користувача на головну сторінку, якщо він вже увійшов
+#         return super().dispatch(request, *args, **kwargs)
+
+#     def get(self, request):
+#         return render(request, self.template_name, context={"form": self.form_class})
+
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data["username"]
+#             messages.success(request, f"Hello {username}, Your account created successfully")
+            
+#             return redirect('quotes:main')  # Надіслати користувача на головну сторінку після успішної реєстрації
+#         return render(request, self.template_name, context={"form": form})
+
+#     def register(request):
+#         if request.method == 'POST':
+#             form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')  # Redirect to the login page after successful registration
+#         else:
+#             form = RegisterForm()
+#         return render(request, 'users/signup.html', {'form': form})
+
+# class LoginView(View):
+    
+#     def index(request):
+#         return render(request, 'index.html')
+    
+#     def get(self, request):
+#         if request.user.is_authenticated:            
+#             return redirect('quotes:main')
+#         return render(request, 'users/login.html', context={"form": LoginForm()})
+
+#     def post(self, request):
+#         user = authenticate(username=request.POST['username'], password=request.POST['password'])
        
-        if user is None:
-            messages.error(request, 'Username or password didn\'t match')
-            return redirect('users:login')
+#         if user is None:
+#             messages.error(request, 'Username or password didn\'t match')
+#             return redirect('users:login')
 
-        login(request, user)
-        return redirect('quotes:main')
+#         login(request, user)
+#         return redirect('quotes:main')
     
-    def login_view(request):
-        if request.method == 'POST':
-        # Handle the login process here
-            pass
-        else:
-            form = AuthenticationForm()
-        return render(request, 'users/login.html', {'form': form})
+#     def login_view(request):
+#         if request.method == 'POST':
+#         # Handle the login process here
+#             pass
+#         else:
+#             form = AuthenticationForm()
+#         return render(request, 'users/login.html', {'form': form})
     
-    template_name = 'users/login.html'
+#     template_name = 'users/login.html'
 
-    def get_success_url(self):
-        return reverse_lazy('index') 
+#     def get_success_url(self):
+#         return reverse_lazy('index') 
+    
+    
+# @login_required
+# def logoutuser(request):
+#     logout(request)
+#     return redirect(to='noteapp:main')
     
 # @login_required
 # def logoutuser(request):
